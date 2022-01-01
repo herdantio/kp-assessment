f7579c1c80eaa46497972822aa16ecad261af955
HERDANTIO <herdantio.sindhunata@binus.ac.id>
2022-01-01 21:48:36 +0700

    add lint create navabr

--- src/components/Pages/Covid.vue
+++ src/components/Pages/Covid.vue
@@ -1,12 +1,14 @@
+<script setup>
+import Navbar from '../organisms/Navbar.vue';
+
+</script>
+
 <template>
   <div>
+      <Navbar></Navbar>
     <h1>Hello World</h1>
   </div>
 </template>
-
-<script setup>
-
-</script>
 
 <style>
 
---
+++ src/components/organisms/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="flex flex-row px-[72px] py-[16px]">
+        <div class="mr-[488px]">
+            <img src="../../assets/kp-logo.PNG" width="150" height="36">
+        </div>
+        <div class="flex flex-row align-middle">
+        <div>
+            <span>
+                Untuk Perusahaan
+            </span>
+        </div>
+        <div>
+            <span>
+                Untuk Klinik
+            </span>
+        </div>
+        </div>
+    </div>
+</template>
