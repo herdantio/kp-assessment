bfd670bb0ec60a858fd4949e61a09d86c015b3d5
HERDANTIO <herdantio.sindhunata@binus.ac.id>
2022-01-01 22:44:00 +0700

    improve navbar

--- src/components/organisms/NavbarTop.vue
+++ src/components/organisms/NavbarTop.vue
@@ -1,19 +1,28 @@
 <template>
-  <div class="px-[72px] py-[16px]">
+  <div class="px-32 py-5">
     <div class="flex flex-row">
-      <div class="mr-[488px]">
-        <img src="../../assets/kp-logo.PNG">
+      <div class="mr-96">
+        <img
+          src="../../assets/kp-logo.PNG"
+          width="150"
+          height="36"
+        >
       </div>
-      <div class="flex flex-row align-middle">
-        <div>
+      <div class="flex flex-row items-center">
+        <div class="mr-5">
           <span>
             Untuk Perusahaan
           </span>
         </div>
-        <div>
+        <div class="mr-5">
           <span>
             Untuk Klinik
           </span>
+        </div>
+        <div>
+          <button class="text-kp-blue border-kp-blue border-2 py-2 px-6 rounded-[4px]">
+            Direktori Klinik
+          </button>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 module.exports = {
   content: [
     './index.html',
-    './src/**/*.{vue,js,ts,jsx,tsx}',
+    './src/**/*.{vue,js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'kp-blue': '#1ab1e5'
+      },
+      colors: {
+        'kp-blue': '#1ab1e5'
+      }
+    }
   },
-  plugins: [],
-};
+  plugins: []
+}
