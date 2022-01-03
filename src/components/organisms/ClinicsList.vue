9f5b4fb98b779f2a13f8bcb4d2c44f1f9267cc14
HERDANTIO <herdantio.sindhunata@binus.ac.id>
2022-01-03 12:20:50 +0700

    add list limit

--- src/components/organisms/ClinicsList.vue
+++ src/components/organisms/ClinicsList.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed, onMounted } from "@vue/runtime-core";
+import { computed, onMounted, ref } from "@vue/runtime-core";
 import { useStore } from "vuex";
 import ClinicUnit from "./ClinicUnit.vue";
 
@@ -12,6 +12,12 @@ onMounted(() => {
 const clinics = computed(() => {
   return store.state.covidModule.clinics
 })
+
+const listLimit = ref(5)
+
+const filterList = computed(() => {
+  return clinics.value.slice(0,listLimit.value)
+})
 </script>
 
 <template>
@@ -20,7 +26,18 @@ const clinics = computed(() => {
             <span class="font-bold">Ada {{clinics.length}} Klinik Sesuai Filter:</span>
         </div>
         <div class="mt-[16px] space-y-4">
-            <ClinicUnit v-for="clinic in clinics" :key="clinic.id" :clinic="clinic" class="p-5"/>
+            <ClinicUnit v-for="clinic in filterList" :key="clinic.id" :clinic="clinic" class="p-5"/>
+        </div>
+
+        <div class="mt-5">
+            <div>
+                <span>Tampilkan</span>
+                <select class="ml-3" v-model="listLimit">
+                    <option :value="5">5</option>
+                    <option :value="10">10</option>
+                    <option :value="20">20</option>
+                </select>
+            </div>
         </div>
     </div>
 </template>
